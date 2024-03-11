c2dce6ba1b4e1e2e6dd808de511f7b3697b2bd33
Benjamin Plante <benplante99@gmail.com>
2024-03-11 15:43:10 -0400

    fixed compile

--- includes/execution.h
+++ includes/execution.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   execution.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bplante <bplante@student.42.fr>            +#+  +:+       +#+        */
+/*   By: bplante <benplante99@gmail.com>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/21 16:03:20 by yothmani          #+#    #+#             */
-/*   Updated: 2024/03/07 14:25:04 by bplante          ###   ########.fr       */
+/*   Updated: 2024/03/11 15:42:27 by bplante          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,6 @@ void	exec_cmd(t_command cmd, char **envp);
 int		exec_builtin(t_command cmd, char **envp);
 void		exec_pwd(char *cmd);
 void		exec_env(t_command *cmd);
-void		exec_echo(t_command cmd);
+void		exec_echo(t_command *cmd);
 // void	exec_non_builtin(t_command cmd, char **envp);
 #endif
