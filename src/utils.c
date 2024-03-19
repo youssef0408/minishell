d591f0d071a27ca5e986f0fd67fb1a631d09527e
youssef0408 <othmaniyoussef97@gmail.com>
2024-03-19 13:38:55 -0400

    put in norm utils.c

--- src/utils.c
+++ src/utils.c
@@ -6,7 +6,7 @@
 /*   By: yothmani <yothmani@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/07 13:59:55 by yothmani          #+#    #+#             */
-/*   Updated: 2024/03/18 16:58:43 by yothmani         ###   ########.fr       */
+/*   Updated: 2024/03/19 13:34:56 by yothmani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,38 +15,6 @@
 bool	is_white_space(char c)
 {
 	return (c == ' ' || (c >= 9 && c <= 13));
-}
-
-char	*trim_str(char *str)
-{
-	int		i;
-	int		j;
-	bool	is_space;
-	char	*trimmed_str;
-
-	i = 0;
-	j = 0;
-	is_space = false;
-	while (is_white_space(str[i]))
-		i++;
-	trimmed_str = calloc(sizeof(char), strlen(str) - i + 1);
-	while (str[i])
-	{
-		if (is_white_space(str[i]))
-			is_space = true;
-		if (!is_white_space(str[i]))
-		{
-			if (is_space)
-			{
-				trimmed_str[j++] = ' ';
-				is_space = false;
-			}
-			trimmed_str[j++] = str[i];
-		}
-		i++;
-	}
-	str = NULL;
-	return (trimmed_str);
 }
 
 void	print_in_color(const char *color, const char *msg)
@@ -74,19 +42,6 @@ int	ft_strcmp(const char *s1, const char *s2)
 		s2++;
 	}
 	return (*s1 - *s2);
-}
-
-void	clean_table(char **tab)
-{
-	int	i;
-
-	i = 0;
-	while (tab[i])
-	{
-		free(tab[i]);
-		i++;
-	}
-	free(tab);
 }
 
 void	exit_prg_at_error(char *str)
